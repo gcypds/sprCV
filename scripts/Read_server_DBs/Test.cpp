96d1395d15e975a0d28ef5a73044239b346343f9
smolinag <santiago.molina.g@gmail.com>
2015-07-23 17:09:20 -0500

    Added Test.cpp

---
+++ scripts/Read_server_DBs/Test.cpp
@@ -0,0 +1,20 @@
+#include "Datasets_Menu.h"
+
+void main()
+{
+	Datasets_Menu M;		
+	
+	M.Select_Dataset();		//Display menu and select video
+
+	Mat F;
+	
+	for(;;)
+	{
+		M.Get_Frame(F);		//Get new frame from selected video
+		
+		if(F.empty())
+			break;
+		
+		waitKey(15);
+	}
+}
